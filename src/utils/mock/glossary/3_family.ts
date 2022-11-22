caafca735587e2b9b28bb91f84cdcbc222345bd6
vaetimidis <othanisushi@gmail.com>
2022-11-22 10:44:49 +0400

    post review fixes

--- src/utils/mock/glossary/3_family.ts
+++ src/utils/mock/glossary/3_family.ts
@@ -2,10 +2,11 @@ import type { IGlossaryItem, IGlossaryContent } from '#/types/glossary';
 
 const text = `<div>
   <span type="text">
-    "Семья" по-китайски - это <span type="h" p="jiā">家</span> Этот иероглиф можно перевести как «семья» или «дом».
+    "Семья" по-китайски - это <span fixed="0" type="h" p="jiā">家</span> Этот иероглиф можно перевести как «семья» или «дом».
     Но истинное значение этого слова гораздо глубже.
-    Иероглиф «семья» – <span type="h" p="jiā">家</span> – обозначает как место, где живет семья, так и людей, которые ее составляют.
-    В верхней части символа находится <span type="h" p="mián">宀</span>, что означает «дом»; в нижней части – <span type="h" p="shǐ">豕</span>, что означает «свинья».
+    Иероглиф «семья» – <span fixed="0" type="h" p="jiā">家</span> – обозначает как место, где живет семья, так и людей, которые ее составляют.
+    В верхней части символа находится <span fixed="0" type="h" p="mián">宀</span>, что означает «дом»;
+    в нижней части – <span fixed="0" type="h" p="shǐ">豕</span>, что означает «свинья».
   </span>
 
   <br />
@@ -20,25 +21,22 @@ const text = `<div>
     <span type="h" p="ru jiā" t="конфуцианская семья.">儒家</span>  <br />
   </span>
 
-  <br />
-  <br />
-  <br />
+  <hr />
 
   <span type="text">Члены семьи:</span> <br /> <br />
-  <div type="example">
-    <span type="h" p="mǔ qīn" t="Мать">母亲</span> <br />
-    <span type="h" p="fù qīn" t="Отец">父亲</span> <br />
-    <span type="h" p="xiōng dì" t="Брат">兄弟</span> <br />
-    <span type="h" p="zǐ mèi" t="Сестра">姊妹</span> <br />
-    <span type="h" p="ér zi" t="Сын">儿子</span> <br />
-    <span type="h" p="nǚ ér" t="Дочь">女儿</span> <br />
-    <span type="h" p="hái zi men" t="Дети">孩子们</span> <br />
-    <span type="h" p="jì fù" t="Отчим">继母</span> <br />
-    <span type="h" p="jì jiě mèi" t="Сводная сестра">继姐妹</span> <br />
-    <span type="h" p="nǚ xù" t="Зять">女婿</span> <br />
-    <span type="h" p="ér xí fù" t="Невестка">儿媳妇</span> <br />
-    <span type="h" p="qī zi" t="Жена">妻子</span> <br />
-    <span type="h" p="zhàng fū" t="Муж">丈夫</span> <br />
+  <div type="example" row="2">
+    <span fixed="1" type="h" p="mǔ qīn" t="Мать">母亲</span> <br />
+    <span fixed="1" type="h" p="fù qīn" t="Отец">父亲</span> <br />
+    <span fixed="1" type="h" p="xiōng dì" t="Брат">兄弟</span> <br />
+    <span fixed="1" type="h" p="ér zi" t="Сын">儿子</span> <br />
+    <span fixed="1" type="h" p="nǚ ér" t="Дочь">女儿</span> <br />
+    <span fixed="1" type="h" p="hái zi men" t="Дети">孩子们</span> <br />
+    <span fixed="1" type="h" p="jì fù" t="Отчим">继母</span> <br />
+    <span fixed="1" type="h" p="jì jiě mèi" t="Сводная сестра">继姐妹</span> <br />
+    <span fixed="1" type="h" p="nǚ xù" t="Зять">女婿</span> <br />
+    <span fixed="1" type="h" p="ér xí fù" t="Невестка">儿媳妇</span> <br />
+    <span fixed="1" type="h" p="qī zi" t="Жена">妻子</span> <br />
+    <span fixed="1" type="h" p="zhàng fū" t="Муж">丈夫</span> <br />
   </div>
 </div>`;
 
