29eee06922f7bd8f2634ad95fd4421a0590404d0
vaetimidis <othanisushi@gmail.com>
2022-12-06 14:10:01 +0400

    feat: ivan zalupa

---
+++ src/utils/mock/glossary/6_le/index.ts
@@ -0,0 +1,26 @@
+import type { IGlossaryBase, IGlossaryContent, IGlossaryItem } from '#/types/glossary';
+import { briefly } from './briefly';
+import { tester } from './tester';
+import { text } from './text';
+
+const base = {
+  id: '6',
+  url: 'le',
+  title: 'Частица 了(le)',
+  description: 'Частица, показывающая изменение ситуации или состояния 了 (le)',
+  hsk: '2'
+} as IGlossaryBase;
+
+export const leGlossary6: IGlossaryContent {
+    ...base,
+    text,
+    briefly,
+    tester
+};
+
+export const leGlossaryItem6: IGlossaryItem {
+    ...base,
+    icon: '1',
+    title: 'Частица 了(le)',
+    badges: ['модальные глаголы']
+}
